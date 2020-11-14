d38cccfbe0b059ff8b2e3b6c2ef14de922d54f49
Nora <Nora@noras-air.home>
2020-11-14 16:05:48 +0100

    update of models.py
    Syntax ausgebessert, Feldtypen angepasst (Bspw. Boolean)

--- Rechnerpraktikum/wienerfestwochen/models.py
+++ Rechnerpraktikum/wienerfestwochen/models.py
@@ -3,28 +3,26 @@ from django.utils import timezone
 
 
 class Team(models.Model):
-    id = models.IntegerField(max_length=4, blank=False, unique=True, verbose_name='TeamID')
-    FS_id =
+    id = models.IntegerField(blank=False, unique=True, primary_key=True, verbose_name='TeamID')
 
     def __str__(self):
         return self.id
 
 
 class Teammitglied(models.Model):
-    id = models.IntegerField(max_length=30, blank=False)
+    id = models.IntegerField(blank=False, unique=True, primary_key=True)
     Fachbereich = models.CharField(max_length=30, blank=False)
-    Stundenlohn = models.DateField(default=timezone.now(), blank=True)
+    Stundenlohn = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
 
     def __str__(self):
         return self.Fachbereich + " " + self.Stundenlohn
 
 
 class Person(models.Model):
-    id = models.IntegerField(max_length=30, blank=False)
+    id = models.IntegerField(blank=False, unique=True, primary_key=True)
     PName = models.CharField(max_length=6, blank=False, default='Pils',
                              choices=[('Pils', 'Pils'), ('Bock', 'Bock'), ('Export', 'Export'), ('Weizen', 'Weizen'), ])
-    ist_extern = models.BooleanField(max_length=2, blank=False',choices=
-                           choices=[('Obergärig', 'Obergärig'), ('Untergärig', 'Untergärig'), ])
+    ist_extern = models.BooleanField(default=False)
     Preis = models.IntegerField(default=5, blank=False, verbose_name='Preis pro Liter')
 
     def __str__(self):
@@ -33,8 +31,8 @@ class Person(models.Model):
 
 class Auftritt(models.Model):
     FS_PNR = models.ForeignKey(Team, on_delete=models.PROTECT, blank=False, verbose_name='Besteller')
-    id = models.IntegerField(default=1, blank=False)
-    Zeit = models.DateTimeField(default=timezone.now(), blank=False)
+    id = models.IntegerField(blank=False, unique=True, primary_key=True)
+    Zeit = models.DateTimeField(default=timezone.now, blank=False)
 
     def __str__(self):
         return ""
